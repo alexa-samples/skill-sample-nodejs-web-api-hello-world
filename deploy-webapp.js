d1adc67ac6d46d5783d269f96ebe43e37c1f225f
Chris Subagio <surjosub@amazon.com>
2022-12-20 16:23:55 -0800

    chore: cleanup
    * removed unnecessary brazil config file
    * switch to us-east-1 for skill infrastructure
    * adding license blocks to all code files
    * fix some wonky formatting

--- deploy-webapp.js
+++ deploy-webapp.js
@@ -1,3 +1,7 @@
+// Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: LicenseRef-.amazon.com.-AmznSL-1.0
+// Licensed under the Amazon Software License  http://aws.amazon.com/asl/
+
 const {execSync} = require('child_process');
 const path = require('path');
 
